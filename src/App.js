78d06e0704a7ea56a3685189a87d44da10fb297c
kbilleaud <kbilleaud@opentable.com>
2018-11-21 18:57:33 -0700

    timer and functions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const {count}=this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div></div>
+          <Clock time={count} />
           <Button label="Break" onClickHandler={this.startBreak.bind(this)} />
           <Button label="Lunch" onClickHandler={this.startLunch.bind(this)} />
           <Button label="Settings" id="settings_button" />
@@ -21,12 +23,23 @@ class App extends Component {
     if(this.timer) {
       clearInterval(this.timer);
       this.setState(
-        {running:true}
+        {
+        running:true,
+        count:1500,
+        }
       );
     }
   }
   startLunch() {
-    console.log('lunch');
+    if(this.timer) {
+      clearInterval(this.timer);
+      this.setState(
+        {
+        running:true,
+        count:6000,
+        }
+      );
+    }
   }
   reset() {
     this.state = {
@@ -48,7 +61,23 @@ class Button extends React.Component {
     );
   }
 }
-
+class Clock extends React.Component {
+  format(time) {
+    let seconds = time % 60;
+    let minutes = Math.floor(time / 60);
+    minutes = minutes.toString().length === 1 ? "0" + minutes : minutes;
+    seconds = seconds.toString().length === 1 ? "0" + seconds : seconds;
+    return minutes + ':' + seconds;
+  }
+  render () {
+    const {time} = this.props;
+    return (
+      <div className="displayedTime">
+        <h1>{this.format(time)}</h1>
+      </div>
+    )
+  }
+}
 
 
 export default App;
