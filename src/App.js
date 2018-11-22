6943ccc4f1e6942ff343093c67844ffd42ace177
kbilleaud <kbilleaud@opentable.com>
2018-11-21 18:36:34 -0700

    css and new functions

--- src/App.js
+++ src/App.js
@@ -8,15 +8,48 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div class="countdown"></div>
-          <button>Break</button>
-          <button>Lunch</button>
-          <button>Settings</button>
+          <div></div>
+          <Button label="Break" onClickHandler={this.startBreak.bind(this)} />
+          <Button label="Lunch" onClickHandler={this.startLunch.bind(this)} />
+          <Button label="Settings" id="settings_button" />
+          <Button label="Reset" id="reset_button" onClickHandler={this.reset.bind(this)} />
         </header>
       </div>
     );
   }
+  startBreak() {
+    if(this.timer) {
+      clearInterval(this.timer);
+      this.setState(
+        {running:true}
+      );
+    }
+  }
+  startLunch() {
+    console.log('lunch');
+  }
+  reset() {
+    this.state = {
+      count:0,
+    }
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      running: false,
+    }
+  }
 }
+class Button extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.onClickHandler} id={this.props.id}>{this.props.label}</button>
+    );
+  }
+}
+
+
 
 export default App;
 /////////////////////////////////////////////////////////////
